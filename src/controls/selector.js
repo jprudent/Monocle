b2a33428e1cbef142e8f8eb10ab1a0b7724f8042
Jérôme Prudent <jprudent@gmail.com>
2011-09-08 00:11:24 +0700

    now we get the selection and its context in a single callback

--- src/controls/selector.js
+++ src/controls/selector.js
@@ -3,12 +3,11 @@
 //This is an invisible control
 //It captures text selection and its context invoke a callback
 
-//textCallback : a function(text) used to retrieve the selected text
+//callback : a function(text,context) used to retrieve the selected text in his 
+//  context
 //paragraphClass : a css class name 
-//paragraphCallback optional function(paragraph) used to retrieve the context of
-//  selected text
 
-Monocle.Controls.Selector = function (reader,textCallback,paragraphClass,paragraphCallback) {
+Monocle.Controls.Selector = function (reader,callback,paragraphClass) {
 
   var API = { constructor: Monocle.Controls.Selector }
   var k = API.constants = API.constructor;
@@ -16,8 +15,7 @@ Monocle.Controls.Selector = function (reader,textCallback,paragraphClass,paragra
   
   function initialize() {
     p.reader = reader;
-    p.textCallback = textCallback;
-    p.paragraphCallback = paragraphCallback;
+    p.callback = callback;
     p.paragraphClass = paragraphClass;
     p.console = console || {
       info: function(){},
@@ -26,27 +24,23 @@ Monocle.Controls.Selector = function (reader,textCallback,paragraphClass,paragra
     };
   } 
 
-  function getSelection(){
-    var t = (this.all) ? this.selection.createRange().text : this.getSelection();
-    p.textCallback(t)
-  }
   
-  function getParagraph(){
+  
+  function callCallback(){
     //note: 'this' is the element that fired the event
+    var txt = (this.all) ? this.ownerDocument.selection.createRange().text : this.ownerDocument.getSelection();  
     var paragraph = this.innerHTML;
-    p.paragraphCallback(paragraph);
+    p.callback(txt,paragraph);
   }
   
   function addSelectorCapacity() {
-    //add mouse up event for all iframes
     var iframes = p.reader.properties.graph.component;
     for(var i=0; i< iframes.length; i++){
       var iframe = iframes[i].contentDocument;
-      Monocle.Events.listen(iframe,"mouseup",getSelection);
       //add mouseup event for all paragraphs
       var paragraphs = iframe.getElementsByClassName(p.paragraphClass);
       for(var j=0; j < paragraphs.length; j++) {
-        paragraphs[j].addEventListener("mouseup",getParagraph)
+        paragraphs[j].addEventListener("mouseup",callCallback)
       }
     }
     p.console.info("added selector capacity to iframes")
